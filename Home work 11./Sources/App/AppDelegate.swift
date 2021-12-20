db9074a10f609bf653c167cf5f12bb73dfbf03ca
Ренат <BOSS.Renat.Safin@gmail.com>
2021-12-20 21:06:22 +0300

    Project setup.

---
+++ Home work 11./Sources/App/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  Home work 11.
+//
+//  Created by Ренат Сафин on 20.12.2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ Home work 11./Sources/Scenes/TimerView.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Home work 11.
+//
+//  Created by Ренат Сафин on 20.12.2021.
+//
+
+import UIKit
+
+class TimerView: UIViewController {
+
+    //MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        setupHierarchy()
+        setupLayout()
+        setupView()
+    }
+
+    //MARK: - Settings
+    
+    private func setupHierarchy() {
+        
+        
+    }
+    
+    private func setupLayout() {
+        
+        
+    }
+    
+    private func setupView() {
+        
+        
+    }
+
+}
+
