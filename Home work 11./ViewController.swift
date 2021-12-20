be8b2883362f582074468c53e7bb8ba04635e109
Ренат <BOSS.Renat.Safin@gmail.com>
2021-12-20 20:21:08 +0300

    Initial Commit

---
+++ Home work 11./ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Home work 11.
+//
+//  Created by Ренат Сафин on 20.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
